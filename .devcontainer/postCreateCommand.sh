366d200823dfa2deea1a916c27b302050656ecd2
tomdot-dev <82123860+tomdot-dev@users.noreply.github.com>
2025-05-22 01:00:36 +0200

    Devcontainer: Add more tools, and move post commands to a script

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+
+sudo apt-get update
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y openvpn php-codesniffer apt-cacher-ng
+sudo apt autoclean
+
+npm install --save-dev stylelint stylelint-config-standard
+npm install --save-dev stylelint-config-standard-scss
