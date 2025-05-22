315a53ca88ebf916604008140eb0c18401ab0708
tomdot-dev <82123860+tomdot-dev@users.noreply.github.com>
2025-05-23 00:01:54 +0200

    Devcontainer: add some more tools

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
 
+CODEQL_BUNDLE_VER=2.21.3
+
 sudo apt-get update
 sudo DEBIAN_FRONTEND=noninteractive \
     apt-get install -y \
@@ -9,8 +11,10 @@ sudo apt autoclean
 
 npm install --save-dev stylelint stylelint-config-standard
 npm install --save-dev stylelint-config-standard-scss
+npm install --save-dev markdownlint-cli2-formatter-sarif
+npm install -g @microsoft/sarif-multitool
 
-pipx install json-spec
+
 python3 -m pip install \
             flake8 flake8-sarif \
             pylint lintrunner-adapters \
@@ -19,3 +23,19 @@ python3 -m pip install \
 
 sudo git clone https://github.com/devcontainers/spec/ /opt/devcontainers-spec
 sudo chown -R vscode:vscode /opt/devcontainers-spec/
+
+wget "https://github.com/github/codeql-action/releases/download/codeql-bundle-v${CODEQL_BUNDLE_VER}/codeql-bundle-linux64.tar.gz" -O /tmp/codeql-bundle-linux64.tar.gz
+sudo tar -zxf /tmp//tmp/codeql-bundle-linux64.tar.gz --directory /opt
+rm /tmp//tmp/codeql-bundle-linux64.tar.gz
+
+# Delete unneeded languages
+# shellcheck disable=SC3009
+sudo rm -rf /opt/codeql/{java,cpp,csharp,go,swift,ruby,Open-Source-Notices,LICENSE.md}
+sudo rm -rf /opt/codeql/tools/java
+
+# shellcheck disable=SC2016
+echo 'PATH="/opt/codeql:$PATH"' >> "${HOME}/.profile"
+# shellcheck disable=SC2016
+echo 'PATH="/opt/codeql:$PATH"' >> "${HOME}/.bashrc"
+# shellcheck disable=SC2016
+echo 'PATH="/opt/codeql:$PATH"' >> "${HOME}/.zshrc"
