f1ae026e57c233ea8782f610e3ccbe564627eb98
morehax <haxinatorgalore@gmail.com>
2025-05-22 15:30:05 -0400

    Minor fixes to the php around nmcli commands and html folder permissions

---
+++ haxinator-pigen-overlay/stage2/02-custom-config/root_files/files/enable_usb_ether.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sed -i 's/console=serial0,115200 console=tty1/console=tty1 console=ttyGS0,115200/' /boot/firmware/cmdline.txt
+sed -i 's/$/ modules-load=dwc2,g_cdc cfg80211.ieee80211_regdom=GB/' /boot/firmware/cmdline.txt
+echo "dtoverlay=dwc2,dr_mode=peripheral" >> /boot/firmware/config.txt
+echo "Reboot and bask in the joy of usb0"
+
