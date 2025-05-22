655daad978c67038f97f30b939a75a75d6b215c6
tomdot-dev <82123860+tomdot-dev@users.noreply.github.com>
2025-05-22 21:45:32 +0200

    Add a few more tools in the devcontainer

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,13 +1,21 @@
 #!/usr/bin/env sh
 
 sudo apt-get update
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -y openvpn php-codesniffer apt-cacher-ng
+sudo DEBIAN_FRONTEND=noninteractive \
+    apt-get install -y \
+        openvpn php-codesniffer apt-cacher-ng file \
+        net-tools whois iodine
 sudo apt autoclean
 
 npm install --save-dev stylelint stylelint-config-standard
 npm install --save-dev stylelint-config-standard-scss
 
+pipx install json-spec
 python3 -m pip install \
             flake8 flake8-sarif \
             pylint lintrunner-adapters \
-            bandit bandit-sarif
+            bandit bandit-sarif \
+            json-spec
+
+sudo git clone https://github.com/devcontainers/spec/ /opt/devcontainers-spec
+sudo chown -R vscode:vscode /opt/devcontainers-spec/
