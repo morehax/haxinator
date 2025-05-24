020210c7bd7ae802326643be460dddaf8b329e38
Yes <screenshotnode@gmail.com>
2025-05-24 12:03:03 +0000

    Minor cleanup of /var/www/html

--- haxinator-pigen-overlay/stage2/02-custom-config/root_files/files/enable_usb_ether.sh
+++ haxinator-pigen-overlay/stage2/02-custom-config/root_files/files/enable_usb_ether.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+#last provisioning and cleanups
+# Enable usb gadget at boot
+
 sed -i 's/console=serial0,115200 console=tty1/console=tty1 console=ttyGS0,115200/' /boot/firmware/cmdline.txt
 sed -i 's/$/ modules-load=dwc2,g_cdc cfg80211.ieee80211_regdom=GB/' /boot/firmware/cmdline.txt
 echo "dtoverlay=dwc2,dr_mode=peripheral" >> /boot/firmware/config.txt
 echo "Reboot and bask in the joy of usb0"
+
+# Clean up web root
+
+rm -rf /var/www/html/index.html
+
+# reboot
 reboot
