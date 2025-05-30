bd134d2334ad594f7d50aa23b00f6064db96831a
tomdot-dev <82123860+tomdot-dev@users.noreply.github.com>
2025-05-30 21:01:48 +0200

    Improve devcontainer

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -6,7 +6,7 @@ sudo apt-get update
 sudo DEBIAN_FRONTEND=noninteractive \
     apt-get install -y \
         openvpn php-codesniffer apt-cacher-ng file \
-        net-tools whois iodine
+        net-tools whois iodine yq xq
 sudo apt autoclean
 
 npm install --save-dev stylelint stylelint-config-standard
@@ -24,9 +24,28 @@ python3 -m pip install \
 sudo git clone https://github.com/devcontainers/spec/ /opt/devcontainers-spec
 sudo chown -R vscode:vscode /opt/devcontainers-spec/
 
-wget "https://github.com/github/codeql-action/releases/download/codeql-bundle-v${CODEQL_BUNDLE_VER}/codeql-bundle-linux64.tar.gz" -O /tmp/codeql-bundle-linux64.tar.gz
-sudo tar -zxf /tmp//tmp/codeql-bundle-linux64.tar.gz --directory /opt
-rm /tmp//tmp/codeql-bundle-linux64.tar.gz
+
+arch="$(uname -m)"
+if [ "${arch}" != "x86_64" ]; then
+    echo 'Package for CodeQL tools are meant for x86_64'
+    exit 0
+fi
+
+CODEQL_TARGZ="/tmp/codeql-bundle-linux64.tar.gz"
+CODEQL_TARGZ_WORKSPACE="/workspaces/${localWorkspaceFolderBasename}/$(basename ${CODEQL_TARGZ})"
+if [ -f "${CODEQL_TARGZ_WORKSPACE}" ]; then
+    cp "${CODEQL_TARGZ_WORKSPACE}" "$(dirname ${CODEQL_TARGZ})"
+    echo "CODEQL Download not needed, file in cache"
+else
+    wget "https://github.com/github/codeql-action/releases/download/codeql-bundle-v${CODEQL_BUNDLE_VER}/codeql-bundle-linux64.tar.gz" -O "${CODEQL_TARGZ}"
+fi
+
+sudo tar -zxf "${CODEQL_TARGZ}" --directory /opt
+rm "${CODEQL_TARGZ}"
+unset CODEQL_TARGZ
+unset CODEQL_BUNDLE_VER
+
+composer global require --no-cache --dev phpstan/phpstan
 
 # Delete unneeded languages
 # shellcheck disable=SC3009
@@ -34,8 +53,8 @@ sudo rm -rf /opt/codeql/{java,cpp,csharp,go,swift,ruby,Open-Source-Notices,LICEN
 sudo rm -rf /opt/codeql/tools/java
 
 # shellcheck disable=SC2016
-echo 'PATH="/opt/codeql:$PATH"' >> "${HOME}/.profile"
+echo 'PATH="/home/vscode/.config/composer/vendor/bin/:/opt/codeql:$PATH"' >> "${HOME}/.profile"
 # shellcheck disable=SC2016
-echo 'PATH="/opt/codeql:$PATH"' >> "${HOME}/.bashrc"
+echo 'PATH="/home/vscode/.config/composer/vendor/bin/:/opt/codeql:$PATH"' >> "${HOME}/.bashrc"
 # shellcheck disable=SC2016
-echo 'PATH="/opt/codeql:$PATH"' >> "${HOME}/.zshrc"
+echo 'PATH="/home/vscode/.config/composer/vendor/bin/:/opt/codeql:$PATH"' >> "${HOME}/.zshrc"
